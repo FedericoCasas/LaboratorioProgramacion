17ecc52f2a679afac9ed93bef947414dc4e17ebf
Fde Notebook de Casa <casasfede175@gmail.com>
2025-05-09 08:58:48 -0300

    Agregar archivos de proyecto.

---
+++ frmAppPrincipal.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace pryInventarioApp
+{
+    partial class frmAppPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrueba = new System.Windows.Forms.Button();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.cboCategorias = new System.Windows.Forms.ComboBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnReporte = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(503, 36);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(108, 23);
+            this.btnPrueba.TabIndex = 0;
+            this.btnPrueba.Text = "btnPrueba";
+            this.btnPrueba.UseVisualStyleBackColor = true;
+            this.btnPrueba.Click += new System.EventHandler(this.btnPrueba_Click);
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(4, 321);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.RowHeadersWidth = 51;
+            this.dgvProductos.RowTemplate.Height = 24;
+            this.dgvProductos.Size = new System.Drawing.Size(870, 195);
+            this.dgvProductos.TabIndex = 1;
+            this.dgvProductos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProductos_CellClick);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(217, 40);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(212, 22);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(217, 90);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(212, 22);
+            this.txtDescripcion.TabIndex = 3;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(217, 140);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(212, 22);
+            this.txtPrecio.TabIndex = 4;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(217, 190);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(212, 22);
+            this.txtStock.TabIndex = 5;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(128, 43);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 7;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(128, 93);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(79, 16);
+            this.lblDescripcion.TabIndex = 8;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(128, 143);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(46, 16);
+            this.lblPrecio.TabIndex = 9;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Location = new System.Drawing.Point(128, 193);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(41, 16);
+            this.lblStock.TabIndex = 10;
+            this.lblStock.Text = "Stock";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(128, 246);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(66, 16);
+            this.lblCategoria.TabIndex = 11;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // cboCategorias
+            // 
+            this.cboCategorias.FormattingEnabled = true;
+            this.cboCategorias.Location = new System.Drawing.Point(217, 243);
+            this.cboCategorias.Name = "cboCategorias";
+            this.cboCategorias.Size = new System.Drawing.Size(212, 24);
+            this.cboCategorias.TabIndex = 12;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(503, 92);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(108, 23);
+            this.btnAgregar.TabIndex = 13;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(503, 148);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(108, 23);
+            this.btnModificar.TabIndex = 14;
+            this.btnModificar.Text = "MODIFICAR";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(503, 204);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(108, 23);
+            this.btnEliminar.TabIndex = 15;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Location = new System.Drawing.Point(503, 260);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(108, 23);
+            this.btnReporte.TabIndex = 16;
+            this.btnReporte.Text = "REPORTE";
+            this.btnReporte.UseVisualStyleBackColor = true;
+            this.btnReporte.Click += new System.EventHandler(this.btnReporte_Click);
+            // 
+            // frmAppPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 528);
+            this.Controls.Add(this.btnReporte);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.cboCategorias);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.btnPrueba);
+            this.Name = "frmAppPrincipal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrueba;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.ComboBox cboCategorias;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnReporte;
+    }
+}
+
---
+++ frmAppPrincipal.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+
+
+
+
+namespace pryInventarioApp
+{
+    public partial class frmAppPrincipal : Form
+    {
+        OleDbConnection conexion;
+
+        public frmAppPrincipal()
+        {
+            InitializeComponent();
+            CargarCategorias();
+            btnPrueba.Enabled = false;
+            CargarProductos();
+
+
+        }
+
+        private void ProbarConexion()
+        {
+            string rutaBD = "BDsem1.mdb"; // ruta del archico en bin/debug
+            string cadenaConexion = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={rutaBD};";
+
+            try
+            {
+                using (OleDbConnection conexion = new OleDbConnection(cadenaConexion))
+                {
+                    conexion.Open();
+                    MessageBox.Show("Conexión exitosa!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar:\n" + ex.Message);
+            }
+        }
+
+        private void btnPrueba_Click(object sender, EventArgs e)
+        {
+            //ProbarConexion(); 
+            CargarProductos();
+        }
+        private void CargarProductos()
+        {
+            string rutaBD = "BDsem1.mdb"; // ruta del archico en bin/debug
+            string cadenaConexion = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={rutaBD};";
+
+            using (OleDbConnection conexion = new OleDbConnection(cadenaConexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    string consulta = @"
+                SELECT Productos.Codigo, Productos.Nombre, Productos.Descripcion, Productos.Precio, Productos.Stock, Categorias.Nombre AS Categoria
+                FROM Productos
+                INNER JOIN Categorias ON Productos.IdCategoria = Categorias.IdCategoria";
+
+                    OleDbCommand comando = new OleDbCommand(consulta, conexion);
+                    OleDbDataReader lector = comando.ExecuteReader();
+
+                    DataTable tabla = new DataTable();
+                    tabla.Load(lector);
+
+                    dgvProductos.DataSource = tabla;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al cargar productos: " + ex.Message);
+                }
+            }
+        }
+        private void CargarCategorias()
+        {
+            string rutaBD = "BDsem1.mdb"; // ruta del archico en bin/debug
+            string cadenaConexion = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={rutaBD};";
+
+            using (OleDbConnection conexion = new OleDbConnection(cadenaConexion))
+            {
+                try
+                {
+                    conexion.Open();
+                    string consulta = "SELECT IdCategoria, Nombre FROM Categorias";
+
+                    OleDbCommand comando = new OleDbCommand(consulta, conexion);
+                    OleDbDataReader lector = comando.ExecuteReader();
+
+                    DataTable tabla = new DataTable();
+                    tabla.Load(lector);
+
+                    cboCategorias.DataSource = tabla;
+                    cboCategorias.DisplayMember = "Nombre";
+                    cboCategorias.ValueMember = "IdCategoria";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al cargar categorías: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string rutaBD = "BDsem1.mdb"; // ruta del archico en bin/debug
+            string cadenaConexion = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={rutaBD};";
+
+            try
+            {
+                using (OleDbConnection conexion = new OleDbConnection(cadenaConexion))
+                {
+                    conexion.Open();
+
+                    string sql = @"INSERT INTO Productos (Nombre, Descripcion, Precio, Stock, IdCategoria)
+                           VALUES (@nombre, @descripcion, @precio, @stock, @idcategoria)";
+
+                    OleDbCommand cmd = new OleDbCommand(sql, conexion);
+                    cmd.Parameters.AddWithValue("@nombre", txtNombre.Text);
+                    cmd.Parameters.AddWithValue("@descripcion", txtDescripcion.Text);
+                    cmd.Parameters.AddWithValue("@precio", Convert.ToDecimal(txtPrecio.Text));
+                    cmd.Parameters.AddWithValue("@stock", Convert.ToInt32(txtStock.Text));
+                    cmd.Parameters.AddWithValue("@idcategoria", cboCategorias.SelectedValue);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Producto agregado correctamente.");
+
+                    CargarProductos(); // Refresca la tabla
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar producto: " + ex.Message);
+            }
+        }
+
+        private void dgvProductos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow fila = dgvProductos.Rows[e.RowIndex];
+
+                txtNombre.Text = fila.Cells["Nombre"].Value.ToString();
+                txtDescripcion.Text = fila.Cells["Descripcion"].Value.ToString();
+                txtPrecio.Text = fila.Cells["Precio"].Value.ToString();
+                txtStock.Text = fila.Cells["Stock"].Value.ToString();
+                cboCategorias.Text = fila.Cells["Categoria"].Value.ToString();
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+            if (dgvProductos.CurrentRow == null)
+            {
+                MessageBox.Show("Seleccioná un producto primero.");
+                return;
+            }
+
+            int codigo = Convert.ToInt32(dgvProductos.CurrentRow.Cells["Codigo"].Value);
+            string rutaBD = "BDsem1.mdb"; // ruta del archico en bin/debug
+            string cadenaConexion = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={rutaBD};";
+
+            try
+            {
+                using (OleDbConnection conexion = new OleDbConnection(cadenaConexion))
+                {
+                    conexion.Open();
+
+                    string sql = @"UPDATE Productos 
+                           SET Nombre = @nombre, Descripcion = @descripcion, Precio = @precio, Stock = @stock, IdCategoria = @idcategoria
+                           WHERE Codigo = @codigo";
+
+                    OleDbCommand cmd = new OleDbCommand(sql, conexion);
+                    cmd.Parameters.AddWithValue("@nombre", txtNombre.Text);
+                    cmd.Parameters.AddWithValue("@descripcion", txtDescripcion.Text);
+                    cmd.Parameters.AddWithValue("@precio", Convert.ToDecimal(txtPrecio.Text));
+                    cmd.Parameters.AddWithValue("@stock", Convert.ToInt32(txtStock.Text));
+                    cmd.Parameters.AddWithValue("@idcategoria", cboCategorias.SelectedValue);
+                    cmd.Parameters.AddWithValue("@codigo", codigo);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Producto modificado.");
+
+                    CargarProductos();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al modificar: " + ex.Message);
+            }
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvProductos.CurrentRow == null)
+            {
+                MessageBox.Show("Seleccioná un producto para eliminar.");
+                return;
+            }
+
+            DialogResult resultado = MessageBox.Show("¿Estás seguro de que querés eliminar este producto?", "Confirmar eliminación", MessageBoxButtons.YesNo);
+
+            if (resultado == DialogResult.Yes)
+            {
+                int codigo = Convert.ToInt32(dgvProductos.CurrentRow.Cells["Codigo"].Value);
+                string rutaBD = "BDsem1.mdb"; // ruta del archico en bin/debug
+                string cadenaConexion = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={rutaBD};";
+
+                try
+                {
+                    using (OleDbConnection conexion = new OleDbConnection(cadenaConexion))
+                    {
+                        conexion.Open();
+
+                        string sql = "DELETE FROM Productos WHERE Codigo = @codigo";
+                        OleDbCommand cmd = new OleDbCommand(sql, conexion);
+                        cmd.Parameters.AddWithValue("@codigo", codigo);
+
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Producto eliminado.");
+
+                        CargarProductos(); // Refresca el DataGridView
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al eliminar: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnReporte_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Archivo de texto (*.txt)|*.txt";
+            saveFile.Title = "Guardar reporte de inventario";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                string rutaBD = "BDsem1.mdb"; // ruta del archico en bin/debug
+                string cadenaConexion = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={rutaBD};";
+
+                try
+                {
+                    using (OleDbConnection conexion = new OleDbConnection(cadenaConexion))
+                    {
+                        conexion.Open();
+
+                        string consulta = @"
+                    SELECT Productos.Codigo, Productos.Nombre, Productos.Precio, Productos.Stock, Categorias.Nombre AS Categoria
+                    FROM Productos
+                    INNER JOIN Categorias ON Productos.IdCategoria = Categorias.IdCategoria";
+
+                        OleDbCommand cmd = new OleDbCommand(consulta, conexion);
+                        OleDbDataReader reader = cmd.ExecuteReader();
+
+                        using (StreamWriter writer = new StreamWriter(saveFile.FileName))
+                        {
+                            writer.WriteLine("Reporte del Inventario");
+                            writer.WriteLine("----------------------------");
+                            writer.WriteLine("Código\tNombre\t\tPrecio\tStock\tCategoría");
+
+                            while (reader.Read())
+                            {
+                                writer.WriteLine($"{reader["Codigo"]}\t{reader["Nombre"],-20}\t${reader["Precio"],6}\t{reader["Stock"],5}\t{reader["Categoria"]}");
+                            }
+
+                            writer.WriteLine();
+                            writer.WriteLine("Fecha del reporte: " + DateTime.Now.ToShortDateString());
+                        }
+
+                        MessageBox.Show("Reporte generado correctamente.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al generar reporte: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        //dgvProductos
+
+    }
+}
